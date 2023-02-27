8040e1c2396f9fbed7b065e2bdeea83f3a360433
FedeAle1525 <fedeale.1525@gmail.com>
2023-02-27 15:16:25 +0100

    AXIOS request emails (through API) from Server

--- js/script.js
+++ js/script.js
@@ -4,7 +4,32 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+      emails: []
     }
+  },
+
+  methods: {
+
+    // Creo un metodo che genera richiesta di una email (tramite API) a Server utilizzando la libreria Axios
+    getEmailFromApi(){
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+      .then( (resp) => {
+
+        // Dalla risposta del Server, recupero il dato che mi serve
+        const result = resp.data.response;
+        this.emails.push(result);
+
+      })
+    }
+  },
+
+  // Utilizzo Hook "mounted" per eseguire metodo al caricamento della pagina
+  mounted(){
+
+    for(let i=0; i < 10; i++){
+      this.getEmailFromApi();
+    }
+    
+
   }
 }).mount('#app')
